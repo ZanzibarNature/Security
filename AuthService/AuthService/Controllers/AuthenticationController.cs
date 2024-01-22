670856c0073a41259c3ec9d9083f50261a428e3e
Yukming <moondrashiwa@gmail.com>
2024-01-22 18:24:58 +0100

    Fix null reference exception in AuthenticationController

--- AuthService/AuthService/Controllers/AuthenticationController.cs
+++ AuthService/AuthService/Controllers/AuthenticationController.cs
@@ -36,7 +36,7 @@ public class AuthenticationController : ControllerBase
         // Exchange authorization code for access token
         var tokenResponse = await _authService.ExchangeCodeForTokenAsync(code);
 
-        if ( !string.IsNullOrEmpty(tokenResponse.AccessToken))
+        if (tokenResponse != null && !string.IsNullOrEmpty(tokenResponse.AccessToken))
         {
             return Ok(tokenResponse);
         }
